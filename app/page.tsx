ef3c4c740ffa9f23c14109a797f6c0622e343d60
oratio100 <oratio100@gmail.com>
2025-03-09 00:58:48 +0900

    [design] 기본 UI 디자인

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import UI from "./ui";
 
 export default function Home() {
-  return <main>TODO LIST</main>;
+  return (
+    <main>
+      <UI />
+    </main>
+  );
 }
